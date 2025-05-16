a6a7e666a12a9755dede45e78cb0f0a79ff119db
Tanveer <mniraza2@gmail.com>
2025-05-16 14:57:21 +0500

    Update turnable-image.jsx

--- src/components/ui/turnable-image.jsx
+++ src/components/ui/turnable-image.jsx
@@ -4,7 +4,7 @@ const TurnableImage = ({ images }) => {
   return (
     <div className="flex-1 relative">
       <div className="flex items-center mb-2 absolute bottom-5 right-5 z-[2]">
-        <img src="../../360-view.png" className="w-12" title="Drag ti view 360 " />
+        <img src="../../360-view.png" className="w-12" title="Drag to view 360 " />
       </div>
       <ReactImageTurntable
         images={images}
