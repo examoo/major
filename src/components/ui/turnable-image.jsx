b3ad88cfb870810a51bbe496d9e8834b74117c02
Tanveer <mniraza2@gmail.com>
2025-05-16 14:27:29 +0500

    add slider

--- src/components/ui/turnable-image.jsx
+++ src/components/ui/turnable-image.jsx
@@ -1,14 +1,34 @@
-import { ReactImageTurntable } from 'react-image-turntable';
+import React, { useState } from "react";
+import { ReactImageTurntable } from "react-image-turntable";
+const TurnableImage = ({ images }) => {
+  const [index, setIndex] = useState(5);
 
+  const handleSliderChange = (e) => {
+    setIndex(Number(e.target.value));
+  };
 
-
-function TurnableImage(props) {
-    return (
+  return (
+    <div className="flex-1 relative">
+      <div className="flex items-center mb-2 absolute bottom-5 right-5 z-[2]">
+        <img src="../../360-view.png" className="w-12"/>
+      </div>
       <ReactImageTurntable
-        images={props?.images}
+        images={images}
+        initialImageIndex={index}
         autoRotate={{ disabled: true }}
-     />
-    );
+      />
+      <input
+        type="range"
+        min={0}
+        max={images.length - 1}
+        step={1}
+        value={index}
+        onChange={handleSliderChange}
+        className="w-full h-2 bg-gray-300 rounded-lg appearance-none pointer-events-auto"
+        style={{ pointerEvents: "auto" }}
+      />
+    </div>
+  );
 }
 
 export default TurnableImage;
